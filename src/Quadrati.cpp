2c8fc4eefab775b9eef7a51322f7b239eeb4c916
Guido d'Amico <guido_damico@yahoo.com>
2022-11-01 11:11:45 -0700

    Rimosso i tabs.

--- src/Quadrati.cpp
+++ src/Quadrati.cpp
@@ -25,102 +25,102 @@ namespace quadrati {
 
         // Check max iterazioni
         if (deco.getIterazione() > MAX_ITERAZIONI){
-        	spdlog::critical("Troppe iterazioni per {0}: {1} oltre il limite, rinuncio!", deco.getObiettivo(), deco.getIterazione());
-        	deco.setCompleta(true);
+            spdlog::critical("Troppe iterazioni per {0}: {1} oltre il limite, rinuncio!", deco.getObiettivo(), deco.getIterazione());
+            deco.setCompleta(true);
             deco.outputCurrentStatus();
 
             return deco;
         }
 
-		// Nuova iterazione
-		deco.setIterazione(deco.getIterazione() + 1);
-		spdlog::debug("Inizio iterazione {0}, livello {1}, obiettivo {2}.", deco.getIterazione(), deco.getLivello(), deco.getObiettivo());
-		//deco.outputCurrentStatus();
+        // Nuova iterazione
+        deco.setIterazione(deco.getIterazione() + 1);
+        spdlog::debug("Inizio iterazione {0}, livello {1}, obiettivo {2}.", deco.getIterazione(), deco.getLivello(), deco.getObiettivo());
+        //deco.outputCurrentStatus();
 
-		// Il livello a cui opero
-		int livelloAttuale = deco.getLivello();
+        // Il livello a cui opero
+        int livelloAttuale = deco.getLivello();
 
-		// Calcola il nuovo valore di questo livello e il corrispondente nuovo resto
-		long nuovoAddendo = stimaNuovoAddendo(deco);
-		deco.setAddendo(livelloAttuale, nuovoAddendo);
+        // Calcola il nuovo valore di questo livello e il corrispondente nuovo resto
+        long nuovoAddendo = stimaNuovoAddendo(deco);
+        deco.setAddendo(livelloAttuale, nuovoAddendo);
 
-		long nuovoResto = deco.getResto();
-		spdlog::debug("Nuovo addendo: {0}, nuovo resto: {1}.", deco.getAddendi()[livelloAttuale], deco.getResto());
+        long nuovoResto = deco.getResto();
+        spdlog::debug("Nuovo addendo: {0}, nuovo resto: {1}.", deco.getAddendi()[livelloAttuale], deco.getResto());
 
-		// Calcola il limite minimo per il prossimo addendo
-		long limite = (long) sqrt(deco.getResto());
-		if (limite > deco.getLimiti()[livelloAttuale]) {
-			deco.setLimite(deco.getLivello(), limite);
-			spdlog::trace("Limite per Livello {0}, Iterazione {1}, nuovo addendo {2}, resto {3}: {4}",
-					deco.getLivello(),
-					deco.getIterazione(),
-					nuovoAddendo,
-					deco.getResto(),
-					limite);
-		}
+        // Calcola il limite minimo per il prossimo addendo
+        long limite = (long) sqrt(deco.getResto());
+        if (limite > deco.getLimiti()[livelloAttuale]) {
+            deco.setLimite(deco.getLivello(), limite);
+            spdlog::trace("Limite per Livello {0}, Iterazione {1}, nuovo addendo {2}, resto {3}: {4}",
+                    deco.getLivello(),
+                    deco.getIterazione(),
+                    nuovoAddendo,
+                    deco.getResto(),
+                    limite);
+        }
 
-		// Controlla dove siamo arrivati
-		if (nuovoResto == 0) {
-			// finito!
-			deco.setCompleta(true);
-			spdlog::info("Completata decomposizione di {0}.", deco.getObiettivo());
+        // Controlla dove siamo arrivati
+        if (nuovoResto == 0) {
+            // finito!
+            deco.setCompleta(true);
+            spdlog::info("Completata decomposizione di {0}.", deco.getObiettivo());
 
-			// Azzera i livelli rimanenti
-			for (int i = deco.getLivello() + 1; i < 4; i++) {
-				deco.setAddendo(i, 0);
-			}
-			spdlog::debug("Azzerato i termini rimasti:");
-			deco.outputCurrentStatus();
+            // Azzera i livelli rimanenti
+            for (int i = deco.getLivello() + 1; i < 4; i++) {
+                deco.setAddendo(i, 0);
+            }
+            spdlog::debug("Azzerato i termini rimasti:");
+            deco.outputCurrentStatus();
 
-		} else {
-			// Resto non nullo: altro giro
-			switch (deco.getLivello()) {
-				case 3:
-					// ultimo livello e decomposizione ancor invalida:
-					// azzera questo livello
-					deco.setAddendo(livelloAttuale, 0);
+        } else {
+            // Resto non nullo: altro giro
+            switch (deco.getLivello()) {
+                case 3:
+                    // ultimo livello e decomposizione ancor invalida:
+                    // azzera questo livello
+                    deco.setAddendo(livelloAttuale, 0);
 
-					// riprova usando un nuovo addendo al livello superiore
-					deco.setLivello(livelloAttuale - 1);
-					deco.outputCurrentStatus();
-					break;
+                    // riprova usando un nuovo addendo al livello superiore
+                    deco.setLivello(livelloAttuale - 1);
+                    deco.outputCurrentStatus();
+                    break;
 
-				case 2:
-				case 1:
-				case 0:
-					/*
-					 * Partendo dal valore attuale e scendendo fino a 1
-					 *     ricorri,
-					 *          se non completa, cala di uno
-					 *          se non puoi calare di uno metti a zero e ritorna
-					 */
-					for (long valoreAttuale = nuovoAddendo; (!deco.getCompleta()) && valoreAttuale > 0 && valoreAttuale >= limite; valoreAttuale--) {
-						// Memorizza il valore più recente al livello attuale
-						deco.getAddendi()[livelloAttuale] = valoreAttuale;
+                case 2:
+                case 1:
+                case 0:
+                    /*
+                     * Partendo dal valore attuale e scendendo fino a 1
+                     *     ricorri,
+                     *          se non completa, cala di uno
+                     *          se non puoi calare di uno metti a zero e ritorna
+                     */
+                    for (long valoreAttuale = nuovoAddendo; (!deco.getCompleta()) && valoreAttuale > 0 && valoreAttuale >= limite; valoreAttuale--) {
+                        // Memorizza il valore più recente al livello attuale
+                        deco.getAddendi()[livelloAttuale] = valoreAttuale;
 
-						// Proviamo a calcolare gli altri addendi dal livello più giù di uno
-						deco.setLivello(livelloAttuale + 1);
+                        // Proviamo a calcolare gli altri addendi dal livello più giù di uno
+                        deco.setLivello(livelloAttuale + 1);
 
-						// Ricorsivamente calcola gli altri addendi
-						deco.outputCurrentStatus();
-						deco = addendi(deco);
-					}
+                        // Ricorsivamente calcola gli altri addendi
+                        deco.outputCurrentStatus();
+                        deco = addendi(deco);
+                    }
 
-					// se il ciclo precendete non ha risolto: azzera il corrente valore e riitorna ad un livello più sù
-					if (!deco.getCompleta()) {
-						deco.getAddendi()[livelloAttuale] = 0;
+                    // se il ciclo precendete non ha risolto: azzera il corrente valore e riitorna ad un livello più sù
+                    if (!deco.getCompleta()) {
+                        deco.getAddendi()[livelloAttuale] = 0;
 
-						// su di un livello
-						deco.setLivello(livelloAttuale - 1);
-					}
-					//deco.outputCurrentStatus();
-					break;
+                        // su di un livello
+                        deco.setLivello(livelloAttuale - 1);
+                    }
+                    //deco.outputCurrentStatus();
+                    break;
 
-				default:
-					spdlog::critical("ERRORE: Livello invalido: {0}.", deco.getLivello());
-					throw 10;
-			}
-		}
+                default:
+                    spdlog::critical("ERRORE: Livello invalido: {0}.", deco.getLivello());
+                    throw 10;
+            }
+        }
         return deco;
     }
 
